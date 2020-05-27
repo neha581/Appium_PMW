d504f973428480cda392b34b81ebcae53bce2c32
neha <nehasharma.neha.ns@gmail.com>
2020-05-27 18:16:53 +0530

    TestNG Integration.

--- appiumtests/src/test/java/appiumtests/Login_Logout.java
+++ appiumtests/src/test/java/appiumtests/Login_Logout.java
@@ -1,15 +1,26 @@
 package appiumtests;
 import java.net.URL;
+//import java.util.concurrent.TimeUnit;
 
+import org.apache.log4j.Logger;
+import org.apache.log4j.xml.DOMConfigurator;
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterMethod;
+//import org.testng.Assert;
+//import org.testng.annotations.*;
+//import org.openqa.selenium.WebDriver;
+//import org.openqa.selenium.firefox.FirefoxDriver;
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.MobileElement;
-
 //import org.openqa.selenium.By;
 import org.openqa.selenium.remote.DesiredCapabilities;
-
+@Test
 public class Login_Logout {
 	static AppiumDriver<MobileElement> driver;
+	 private static Logger Log = Logger.getLogger(Login_Logout.class.getName());
 	public static void main(String[] args) {
+		DOMConfigurator.configure("log4j.xml");
 		try {
 			openPickmywork();
 		} catch (Exception e) {
@@ -20,6 +31,8 @@ public class Login_Logout {
 		}
 		// TODO Auto-generated method stub
 }
+	@BeforeMethod
+	
 	public static void openPickmywork() throws Exception
 	{
 	DesiredCapabilities cap = new DesiredCapabilities();
@@ -36,61 +49,63 @@ public class Login_Logout {
 	cap.setCapability("clearSystemFiles",true);
 	cap.setCapability("autoGrantPermissions",true);
 	URL url = new URL("http://127.0.0.1:4723/wd/hub");
-
+	
 	driver = new AppiumDriver<MobileElement>(url, cap);
+	Log.info("New driver instantiated");
 	System.out.println("Application started...");
 	Thread .sleep(12000);
 	
+	
 	MobileElement choose_lang  = (MobileElement) driver.findElementByAccessibilityId("choose_language");
-	Thread .sleep(12000);
+	Thread .sleep(1200);
 	choose_lang.click();
 	System.out.println("choose_lang");
-	Thread .sleep(12000);
+	Thread .sleep(1200);
 	MobileElement language = (MobileElement) driver.findElementByAccessibilityId("lang_3");
-	Thread .sleep(12000);
+	Thread .sleep(1200);
 	language.click();
 	System.out.println("Telugu");
-	Thread .sleep(12000);
+	Thread .sleep(1200);
 	MobileElement choose_lang1  = (MobileElement) driver.findElementByAccessibilityId("choose_language");
-	Thread .sleep(12000);
+	Thread .sleep(1200);
 	choose_lang1.click();
 	System.out.println("choose_lang");
-	Thread .sleep(12000);
+	Thread .sleep(1200);
 	MobileElement hindi = (MobileElement) driver.findElementByAccessibilityId("lang_2");
-	Thread .sleep(12000);
+	Thread .sleep(1200);
 	hindi.click();
 	System.out.println("language selected.");
-	Thread .sleep(12000);
+	Thread .sleep(1200);
 	MobileElement choose_lang2  = (MobileElement) driver.findElementByAccessibilityId("choose_language");
-	Thread .sleep(12000);
+	Thread .sleep(1200);
 	choose_lang2.click();
 	System.out.println("choose_lang");
-	Thread .sleep(12000);
+	Thread .sleep(1200);
 	
 	MobileElement English = (MobileElement) driver.findElementByAccessibilityId("lang_1");
-	Thread .sleep(12000);
+	Thread .sleep(1200);
 	English.click();
 	System.out.println("select english as app language.");
-	Thread .sleep(12000);
+	Thread .sleep(1200);
 	
 	MobileElement verify_number = (MobileElement) driver.findElementByAccessibilityId("intro_3");
-	Thread.sleep(12000);
+	Thread.sleep(1200);
 	verify_number.click();
 	System.out.println("button:verify mobile number.");
-	Thread.sleep(12000);
+	Thread.sleep(1200);
 	
 	MobileElement mobileNumber = (MobileElement)driver.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.EditText");
-	Thread.sleep(12000);   
+	Thread.sleep(1200);   
 	mobileNumber.sendKeys("7976890438");
 	System.out.println("number entered successfully.");
-
+	Thread.sleep(1200);
 	MobileElement submit = (MobileElement) driver.findElementByAccessibilityId("login_1");
 	submit.click();
 	System.out.println("number submitted successfully.");
 	Thread.sleep(12000);
 	
 	MobileElement submitotp = (MobileElement) driver.findElementByAccessibilityId("login_8");
-	Thread.sleep(12000);
+	Thread.sleep(1200);
 	submitotp.click();
 	System.out.println("otp submitted successfully.");
 	Thread.sleep(12000);
@@ -101,8 +116,13 @@ public class Login_Logout {
 	System.out.println("go to my profile");
 	Thread.sleep(12000);
 	MobileElement logout = (MobileElement) driver.findElementByAccessibilityId("profile_5");
-	Thread.sleep(12000);
+	Thread.sleep(1200);
 	logout.click();
 	System.out.println("logout");
 	}
+	 @AfterMethod
+	 public void afterMethod() {
+	 
+	 }
+	 
 }
