dfd7fb6868f8482a846c263c2c8588b9eb2e95d9
neha581 <nehasharma.neha.ns@gmail.com>
2020-11-11 13:49:28 +0530

    new tc

---
+++ appiumtests/src/main/java/appiumtests/Login_LogoutTest.java
@@ -0,0 +1,11 @@
+package appiumtests;
+
+import org.testng.annotations.Test;
+
+public class Login_LogoutTest {
+
+  @Test
+  public void openPickmywork() {
+    throw new RuntimeException("Test not implemented");
+  }
+}
