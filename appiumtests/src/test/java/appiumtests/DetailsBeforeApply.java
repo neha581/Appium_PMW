03e308ecd521556e09141ff260ab48a4af410bfd
neha581 <nehasharma.neha.ns@gmail.com>
2020-11-19 12:14:39 +0530

    adding mandatory details before apply.

---
+++ appiumtests/src/test/java/appiumtests/DetailsBeforeApply.java
@@ -0,0 +1,99 @@
+package appiumtests;
+import java.net.URL;
+
+import org.apache.log4j.Logger;
+import org.apache.log4j.xml.DOMConfigurator;
+import org.testng.annotations.Test;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
+
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+public class DetailsBeforeApply {
+	static AppiumDriver<MobileElement> driver;
+	 private static Logger Log = Logger.getLogger(Login_Logout.class.getName());
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		DOMConfigurator.configure("log4j.xml");
+		try {
+			openPickmywork();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			System.out.println(e.getCause());
+		System.out.println(e.getMessage());
+			e.printStackTrace();
+		}
+
+	}
+	@Test
+	public static void openPickmywork() throws Exception
+	{
+	DesiredCapabilities cap = new DesiredCapabilities();
+	cap.setCapability("newCommandTimeout", 100000);
+	cap.setCapability("avdLaunchTimeout",90000);
+	cap.setCapability("avdReadyTimeout",60000);
+	cap.setCapability("deviceName","Android SDK build for x86");
+	cap.setCapability("udid","emulator-5554");
+	cap.setCapability("platformName","Android");
+	cap.setCapability("platformVersion","9.0.0");
+	cap.setCapability("appPackage","com.pickmywork");
+	cap.setCapability("appActivity","com.pickmywork.MainActivity");
+	cap.setCapability("automationName","UiAutomator2");
+	cap.setCapability("clearSystemFiles",true);
+	cap.setCapability("autoGrantPermissions",true);
+	URL url = new URL("http://127.0.0.1:4723/wd/hub");
+	
+	driver = new AppiumDriver<MobileElement>(url, cap);
+	Log.info("New driver instantiated");
+	System.out.println("Application started...");
+	Thread.sleep(12000);
+	
+	MobileElement verify_number = (MobileElement) driver.findElementByAccessibilityId("intro_3");
+	verify_number.click();
+	System.out.println("verify mobile number.");
+	Thread.sleep(4000);
+	
+	MobileElement mobileNumber = (MobileElement)driver.findElementByAccessibilityId("mobile_id");
+	Thread.sleep(1200);
+	mobileNumber.sendKeys("7976890438");
+	System.out.println("number entered successfully.");
+	
+	MobileElement submit = (MobileElement) driver.findElementByAccessibilityId("login_1");
+	submit.click();
+	System.out.println("number submitted successfully.");
+	Thread.sleep(12000);
+	MobileElement submitotp = (MobileElement) driver.findElementByAccessibilityId("login_8");
+	submitotp.click();
+	Thread.sleep(2000);
+	System.out.println("otp submitted successfully.");
+	Thread.sleep(20000);
+	MobileElement jobcard = (MobileElement) driver.findElementByAccessibilityId("jobcard_15");
+	Thread.sleep(2000);
+	jobcard.click();
+	Thread.sleep(2000);
+	System.out.println("Jobcard clicked");
+	Thread.sleep(8000);
+	
+	MobileElement apply = (MobileElement) driver.findElementByAccessibilityId("apply_4");
+	Thread.sleep(8000);
+	apply.click();
+	Thread.sleep(2000);
+	System.out.println("apply button clicked.");
+	System.out.println("your profile is incomplete");
+	Thread.sleep(12000);
+	MobileElement apply1 = (MobileElement) driver.findElementByAccessibilityId("apply_3");
+	Thread.sleep(8000);
+	apply1.click();
+	Thread.sleep(2000);
+	System.out.println("profile details filled.");
+	MobileElement apply2 = (MobileElement) driver.findElementByAccessibilityId("apply_4");
+	Thread.sleep(8000);
+	apply2.click();
+	Thread.sleep(2000);
+	System.out.println("applied.");
+	
+	}
+
+}
